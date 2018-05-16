807734d8e40e97c6a98d975ba6eba1a1d763494a
Rohit Gupta <rgupta91@uw.edu>
2018-05-15 19:26:33 -0700

    Function with error handling incorporated

--- pybcoin/utils/regex_btc_tweets.py
+++ pybcoin/utils/regex_btc_tweets.py
@@ -14,24 +14,33 @@ data_frame = pd.read_csv('btc_tweets.txt', sep = "],", header=None, engine = 'py
 
 #defining the function to extract date
 def date_func(pass_row):
-    var_str = pass_row.to_string()
-    pattern = r'"([A-Za-z0-9_\./\\-]*)"'
-    m = re.search(pattern, var_str)
-    date_str = m.group()
-    date_str = date_str.replace('"', '')
-    date_str = datetime.datetime.strptime(date_str, '%Y/%m/%d').date()
-    return date_str
+    try:
+        var_str = pass_row.to_string()
+        pattern = r'"([A-Za-z0-9_\./\\-]*)"'
+        m = re.search(pattern, var_str)
+        date_str = m.group()
+        date_str = date_str.replace('"', '')
+        date_str = datetime.datetime.strptime(date_str, '%Y/%m/%d').date()
+        return date_str
+    except ValueError:
+        print ('Date needs to be in a %Y/%m/%d format')
+    except AttributeError:
+        print ('Date needs to be supplied within double quotes as part of a string')
 
 #defining the function to extract num tweets
+#defining the function to extract num tweets
 def num_tweets_func(pass_row):
-    num_str = pass_row.to_string()
-    num_str = re.findall(r',(\w+)', num_str)
-    num_str = num_str[0]
-    if num_str == 'null':
-        num_str = 0
-    else:
-        num_str = int(num_str.encode('ascii'))
-    return num_str
+    try:
+        num_str = pass_row.to_string()
+        num_str = re.findall(r',(\w+)', num_str)
+        num_str = num_str[0]
+        if num_str == 'null':
+            num_str = 0
+        else:
+            num_str = int(num_str.encode('ascii'))
+        return num_str
+    except:
+        print ('Number of tweets to be supplied between commas in a string object')
 
 #creating the final dataframe
 dates_s = data_frame.apply(date_func)
