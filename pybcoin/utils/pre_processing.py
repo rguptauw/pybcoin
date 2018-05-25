e3575bbea1833002a2a843208292dd20c8c36692
pshivraj10 <pshivraj@uw.edu>
2018-05-25 01:38:39 -0700

    Add sentiment module

--- pybcoin/utils/pre_processing.py
+++ pybcoin/utils/pre_processing.py
@@ -7,37 +7,34 @@ from nltk.corpus import stopwords
 
 import pandas as pd
 import re
+import string
+
 # nltk.download('stopwords')
 stops = set(stopwords.words("english"))
 stops.update(['http','https','www','com'])
 
-def read_data():
-    file_list = glob.glob("output_file*")
-    raw_data = pd.DataFrame()
-    dfs = []
-    for file_ in file_list:
-        df = pd.read_table(file_, delimiter=None)
-        dfs.append(df)
-    raw_data = pd.concat(dfs)
+def read_data(path):
+    df = pd.read_csv(path)
+    df['text'] = df['text'].astype(str)
+    return df
 
-    semi_counts = raw_data[raw_data.columns[0]].apply(_count_semi)
-    clean_raw_data = raw_data[raw_data.columns[0]][semi_counts == 9]
-
-    clean_raw_data_split = clean_raw_data.str.split(";").tolist()
-
-    return pd.DataFrame(clean_raw_data_split, columns=raw_data.columns[0].split(";"))
-
-
-def _count_semi(tweet):
-    return Counter(str(tweet))[';']
-
+def remove_urls(tweet):
+    tweet = re.sub(r"http\S+", "", tweet)
+    tweet = re.sub(r'(?:(?:http|https|https://www|https://|http://|):\/\/)'
+                   '?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@:'
+                   '%_\+.~#?&//=]*)?',"",tweet,flags=re.MULTILINE)
+    tweet = '\n'.join([a for a in tweet.split("\n") if a.strip()])
+    return tweet
 
 def clean_tweet(tweet):
-    '''
-    Utility function to clean the text in a tweet by removing
-    links and special characters using regex.
-    '''
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    tweet = tweet.lower()
+    tweet = re.sub('@$[^\s]+','',tweet)
+    tweet = re.sub('[\s]+', ' ', tweet)
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    tweet = tweet.strip('\'"')
+    tweet = re.sub(r'[^\x00-\x7f]',r'', tweet)
+    tweet = re.sub('[0-9]+', '', tweet)
+    return tweet
 
 
 def remove_stopwords(tweet):
@@ -45,10 +42,19 @@ def remove_stopwords(tweet):
     filtered_words = [word for word in tweet.split() if word not in stops]
     return " ".join(filtered_words)
 
+def remove_delimiters(tweet):
+    delimiters = ",.!?/&-:;@'+% =>_..."
+    return ' '.join(w for w in re.split("["+"\\".join(delimiters)+"]", tweet) if w)
 
-if __name__ == '__main__':
-    twitter_data = read_data()
+def remove_lessthantwo(tweet):
+    return ' '.join( [w for w in tweet.split() if len(w)>2] )
+
+def pre_process_data(path):
+    twitter_data = read_data(path)
+    twitter_data['text'] = twitter_data['text'].apply(remove_urls)
     twitter_data['text'] = twitter_data['text'].apply(str.lower)
     twitter_data['text'] = twitter_data['text'].apply(clean_tweet)
     twitter_data['text'] = twitter_data['text'].apply(remove_stopwords)
-    twitter_data.to_csv('processed_data.csv', index=False)
+    twitter_data['text'] = twitter_data['text'].apply(remove_delimiters)
+    twitter_data['text'] = twitter_data['text'].apply(remove_lessthantwo)
+    return twitter_data
