891ea365b3c475385623c559376c9822d5b07b56
pshivraj10 <pshivraj@uw.edu>
2018-05-27 22:00:01 -0700

    Added Docstrings

--- pybcoin/SentimentAnalyzer/sentiment_scorer.py
+++ pybcoin/SentimentAnalyzer/sentiment_scorer.py
@@ -1,4 +1,3 @@
-#requests, vaderSentiment, nltk
 import pandas as pd
 import numpy as np
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -7,32 +6,41 @@ import sys
 import os
 __directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__directory + "/..")
-# sys.path.append(__directory + "/../..")
 from utils.pre_processing import pre_process_data
 
 class SentimentAnalyzer(object):
 
     """
-        The controller object for data collection pipeline. Responsible
-        for triggering and monitoring the daily streaming data collection
-        from all data sources.
+        The controller object for Sentiment Analysis pipeline. Responsible
+        for providing daily sentiment count.
             :member attributes: logger: logger instance
                                 config: config file path
-            :member functions: data_collect_pipeline
+            :member functions: sentiment_scorer
     """
     def __init__(self):
         self.logger = logging.getLogger('simpleExample')
         self.analyzer = SentimentIntensityAnalyzer()
 
     def sentiment_scorer(self, path, keyword = 'twitter'):
+                """
+                    Method to Return Daily Positive and Negative sentiment
+                    : param self
+                    : return text_df(pandas Dataframes)
+                """
 
-        def sentiment_count(grp):
-            pos_count = np.sum(grp['Sentiment']>0)
-            neg_count = np.sum(grp['Sentiment']<0)
-            return pd.DataFrame({
+        def sentiment_count(grouped_Data):
+            """
+                Method to convert sentiment as features.
+                : param self
+                : return grouped_sentiment(pandas Dataframes)
+            """
+            pos_count = np.sum(grouped_Data['Sentiment']>0)
+            neg_count = np.sum(grouped_Data['Sentiment']<0)
+            grouped_sentiment = pd.DataFrame({
                 'pos_count':[pos_count],
                 'neg_count':[neg_count]
             })
+            return grouped_sentiment
 
         text_data = pre_process_data(path)
         sentiment = []
@@ -41,12 +49,9 @@ class SentimentAnalyzer(object):
         for i in range(0, len(text_data)):
             sentiment.append(self.analyzer.polarity_scores(text_data.iloc[i]['text'])['compound'])
             date.append(pd.to_datetime(text_data.iloc[i]['Date']).date())
-            # date.append(text_data.iloc[i]['Date'])
         text_df = pd.DataFrame({'Date' : date,
                                 'Sentiment': sentiment
                                })
-        #text_df['Sentiment'] = np.where(text_df['Sentiment']>0, 'Positive', 'Negative')
-
         text_df = text_df.groupby(['Date']).apply(sentiment_count).reset_index().drop(['level_1'],axis = 1)
         text_df.to_csv('./data/latest/' + keyword + '_sentiment.csv',index=True)
 
--- pybcoin/utils/pre_processing.py
+++ pybcoin/utils/pre_processing.py
@@ -14,47 +14,84 @@ stops = set(stopwords.words("english"))
 stops.update(['http','https','www','com'])
 
 def read_data(path):
-    df = pd.read_csv(path)
-    df['text'] = df['text'].astype(str)
-    return df
+    """
+        Utility function to read csv for text pre-processing.
+        : param path
+        : return text_data(pandas Dataframes)
+    """
+    text_data = pd.read_csv(path)
+    text_data['text'] = text_data['text'].astype(str)
+    return text_data
 
-def remove_urls(tweet):
-    tweet = re.sub(r"http\S+", "", tweet)
-    tweet = re.sub(r'(?:(?:http|https|https://www|https://|http://|):\/\/)'
+def remove_urls(text):
+    """
+        Utility function to clean the text by removing urls.
+        : param path
+        : return text(str)
+    """
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r'(?:(?:http|https|https://www|https://|http://|):\/\/)'
                    '?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@:'
-                   '%_\+.~#?&//=]*)?',"",tweet,flags=re.MULTILINE)
-    tweet = '\n'.join([a for a in tweet.split("\n") if a.strip()])
-    return tweet
+                   '%_\+.~#?&//=]*)?',"",text,flags=re.MULTILINE)
+    text = '\n'.join([a for a in tweet.split("\n") if a.strip()])
+    return text
 
-def clean_tweet(tweet):
-    tweet = tweet.lower()
-    tweet = re.sub('@$[^\s]+','',tweet)
-    tweet = re.sub('[\s]+', ' ', tweet)
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    tweet = tweet.strip('\'"')
-    tweet = re.sub(r'[^\x00-\x7f]',r'', tweet)
-    tweet = re.sub('[0-9]+', '', tweet)
-    return tweet
+def clean_text(text):
+    """
+        Utility function to clean the text by removing special characters.
+        : param text
+        : return text(str)
+    """
+    text = text.lower()
+    text = re.sub('@$[^\s]+','',text)
+    text = re.sub('@$[^\s]+','',text)
+    text = re.sub('[\s]+', ' ', text)
+    text = re.sub(r'#([^\s]+)', r'\1', text)
+    text = text.strip('\'"')
+    text = re.sub(r'[^\x00-\x7f]',r'', text)
+    text = re.sub('[0-9]+', '', text)
+    return text
 
 
-def remove_stopwords(tweet):
+def remove_stopwords(text):
+    """
+        Utility function to clean the text in a tweet by removing english stopwords.
+        : param text
+        : return text(str)
+    """
     # Removing all the stopwords
-    filtered_words = [word for word in tweet.split() if word not in stops]
+    filtered_words = [word for word in text.split() if word not in stops]
     return " ".join(filtered_words)
 
-def remove_delimiters(tweet):
+def remove_delimiters(text):
+    """
+        Utility function to clean the text in a tweet by removing delimiters
+        : param text
+        : return text(str)
+    """
     delimiters = ",.!?/&-:;@'+% =>_..."
-    return ' '.join(w for w in re.split("["+"\\".join(delimiters)+"]", tweet) if w)
+    return ' '.join(w for w in re.split("["+"\\".join(delimiters)+"]", text) if w)
 
-def remove_lessthantwo(tweet):
-    return ' '.join( [w for w in tweet.split() if len(w)>2] )
+def remove_lessthanthree(text):
+    """
+        Utility function to clean the text in a tweet by removing less than less than three
+        letter words.
+        : param text
+        : return text_data(pandas Dataframes)
+    """
+    return ' '.join( [w for w in text.split() if len(w)>2] )
 
 def pre_process_data(path):
-    twitter_data = read_data(path)
-    twitter_data['text'] = twitter_data['text'].apply(remove_urls)
-    twitter_data['text'] = twitter_data['text'].apply(str.lower)
-    twitter_data['text'] = twitter_data['text'].apply(clean_tweet)
-    twitter_data['text'] = twitter_data['text'].apply(remove_stopwords)
-    twitter_data['text'] = twitter_data['text'].apply(remove_delimiters)
-    twitter_data['text'] = twitter_data['text'].apply(remove_lessthantwo)
-    return twitter_data
+    """
+        Utility function to clean the text.
+        : param path
+        : return text_data(pandas Dataframes)
+    """
+    text_data = read_data(path)
+    text_data['text'] = text_data['text'].apply(remove_urls)
+    text_data['text'] = text_data['text'].apply(str.lower)
+    text_data['text'] = text_data['text'].apply(clean_text)
+    text_data['text'] = text_data['text'].apply(remove_stopwords)
+    text_data['text'] = text_data['text'].apply(remove_delimiters)
+    text_data['text'] = text_data['text'].apply(remove_lessthantwo)
+    return text_data
